074e86fcb5a2aa4db483dc922988247e26a12332
Mikhail Vasyak <miahasik@gmail.com>
2026-06-29 20:50:35 +0900

    Мелкие правки

--- js/main.js
+++ js/main.js
@@ -179,3 +179,27 @@ function clearError(input) {
   const error = input.previousElementSibling;
   if (error) error.textContent = '';
 }
+
+let lastKey = null;
+
+function gazan() {
+  const audio = new Audio('/music/music.mp3');
+  audio.play();
+  setTimeout(() => {
+    audio.pause();
+    document.querySelector('img.gazan').style.display = 'none';
+  }, 5000);
+
+  gazanBtn = document.querySelector('.nav__container');
+  gazanBtn.innerHTML += '<img class="gazan" src="/img/features/gazan.png"></img>';
+
+  lastKey = null;
+}
+
+document.addEventListener('keydown', e => {
+  if (lastKey === '6' && e.key === '7') {
+    gazan();
+  } else {
+    lastKey = e.key;
+  }
+});
